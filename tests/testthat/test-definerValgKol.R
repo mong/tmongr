764916d0da0c62b2affb175993db61b696337cb6
arnfinn <arnfinn.steindal@gmail.com>
2017-11-29 18:10:13 +0100

    (hopefully) complete test of definerValgKol.R

--- tests/testthat/test-definerValgKol.R
+++ tests/testthat/test-definerValgKol.R
@@ -1,59 +1,17 @@
 context("definerValgKol")
 
 test_that("definerValgKol returns correct values", {
-  valg_boomr <- c(
-    "Opptaksområde Sykehus" = "boomr_sykehus",
-    "Opptaksområde HF" = "boomr_HF",
-    "Opptaksområde RHF" = "boomr_RHF")
-  valg_aar <- c("År" = "aar")
-  valg_kontakter <- c("Kontakter"="kontakter")
-  valg_rate <- c("Rater"="rate")
-  valg_liggetid <- c("Liggedøgn" = "liggetid")
-  valg_liggeindex <- c("Liggedøgn pr. pasient" = "liggedognindex")
-  valg_drg <- c("DRG-poeng"="drg_poeng")
-  valg_drgrate <- c("DRG-poengrater"="drgrate")
-  valg_drgindex <- c("DRG-index"="drg_index")
-  valg_alder <- c("Alder" = "alder")
-  valg_kjonn <- c("Kjønn" = "kjonn")
-  valg_behandlingsniva <- c("Behandlingsnivå" = "behandlingsniva")
-  valg_DRGtypeHastegrad <- c("DRGtypeHastegrad")
-  valg_Behandler <- c("Behandler")
-  valg_behsh <- c("Behandlende sykehus" = "behandlende_sykehus")
-  valg_behhf <- c("Behandlende HF" = "behandlende_HF")
-  valg_behrhf <- c("Behandlende RHF" = "behandlende_RHF")
-  valg_hdg <- c("Hoveddiagnosegruppe")
-  valg_liggerate <- c("Liggedøgnsrate" = "liggedognrate")
-  valg_icd10 <- c("ICD10-kapittel" = "ICD10Kap")
-
-  valg_en <- c(
-    valg_boomr,
-    valg_aar,
-    valg_behandlingsniva,
-    valg_icd10,
-    valg_Behandler,
-    valg_behhf,
-    valg_behrhf
-  )
-
-  valg_to <- c(
-    valg_Behandler,
-    valg_behhf,
-    valg_behrhf,
-    valg_aar,
-    valg_behandlingsniva,
-    valg_icd10,
-    valg_boomr,
-    "Tom" = "ingen"
-  )
-
-  valg_tre <- c(
-    valg_aar,
-    valg_behandlingsniva,
-    valg_icd10
-  )
-
-  expect_equal(valg_en, definerValgKol(testdata,1))
-  expect_equal(valg_to, definerValgKol(testdata,2))
-  expect_equal(valg_tre, definerValgKol(testdata,3))
+  expect_equal_to_reference(definerValgKol(testdata,1), "data/defvalg1")
+  expect_equal_to_reference(definerValgKol(testdata,2), "data/defvalg2")
+  expect_equal_to_reference(definerValgKol(testdata,3), "data/defvalg3")
+  expect_equal_to_reference(definerValgKol(testdata,4), "data/defvalg4")
+  
+  expect_equal_to_reference(definerValgKol(testdata2,1), "data/defvalg1alt")
+  expect_equal_to_reference(definerValgKol(testdata2,2), "data/defvalg2alt")
+  expect_equal_to_reference(definerValgKol(testdata2,3), "data/defvalg3alt")
+  expect_equal_to_reference(definerValgKol(testdata2,4), "data/defvalg4alt")
+  expect_null(definerValgKol(testdata,5))
+  expect_null(definerValgKol(testdata2,5))
+  
 })
 

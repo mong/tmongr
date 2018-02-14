5aa6549f901e0bfe483993f3b4debec4c5cee00a
Arnfinn <arnfinn.steindal@gmail.com>
2018-02-14 13:50:12 +0100

    try to make variable global by using <<-

--- R/launch_application.R
+++ R/launch_application.R
@@ -1,10 +1,12 @@
 #' @export
-launch_application <- function(minedata=NULL){
+launch_application <- function(minedata = NULL){
   print(minedata)
+  minedata <<- minedata
   shiny::runApp(appDir = system.file("application", package = "dynamiskTabellverk"))
 }
 
 #' @export
-submit_application <- function(){
+submit_application <- function(minedata = NULL){
+  minedata <<- minedata
   rsconnect::deployApp(appDir = system.file("application", package = "dynamiskTabellverk"))
 }

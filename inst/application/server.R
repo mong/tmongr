23bd838f5524f5dce5a5b338ea5473770466edb5
Arnfinn <arnfinn.steindal@gmail.com>
2018-04-26 12:26:37 +0200

    minor bug fix

--- inst/application/server.R
+++ inst/application/server.R
@@ -214,7 +214,7 @@ shinyServer(
     output$datasett <- renderUI({
       if (length(listeDatasett)>1){
         radioButtons("datasett", 
-                     label = NULL
+                     label = NULL,
                      choices = listeDatasett,
                      selected = listeDatasett[1]
         )

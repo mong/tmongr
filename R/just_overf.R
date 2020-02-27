fda3128ca4421dbd786be9798dd75646d2b333d9
arnfinn <arnfinn.steindal@gmail.com>
2020-02-27 15:15:33 +0100

    minor doc update

--- R/just_overf.R
+++ R/just_overf.R
@@ -1,7 +1,6 @@
-
-#' Title
+#' Module for the "adjust for transfers" button
 #'
-#' @param id 
+#' @param id Shiny id
 #'
 #' @return
 #' @export
@@ -11,12 +10,12 @@ just_overfUI <- function(id) {
     shiny::uiOutput(ns("just_overf"))
 }
 
-#' Title
+#' Server side of the "adjust for transfers" button module
 #'
-#' @param input 
-#' @param output 
-#' @param session 
-#' @param colnames 
+#' @param input internal
+#' @param output internal
+#' @param session internal
+#' @param colnames Column names in the data
 #'
 #' @return
 #' @export
@@ -25,7 +24,7 @@ just_overf <- function(input, output, session, colnames) {
     output$just_overf <- shiny::renderUI({
       if ("niva" %in% colnames) {
         shiny::tags$div(title = "Juster for overføringer mellom sykehus.
-Ved justering for overføringer er alle døgn- og dagopphold nær i tid regnet som ett opphold, uavhengig av hvor pasienten er behandlet.",
+Ved justering for overføringer er alle døgn- og dagopphold nær i tid regnet som ett opphold, uavhengig av hvor pasienten er behandlet.",# nolint
                  shinyWidgets::materialSwitch(inputId = "overf",
                                               label = "Juster for overføringer",
                                               value = FALSE,

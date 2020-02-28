db43a2447552e86fb0bc4847e029a679610f35b2
arnfinn <arnfinn.steindal@gmail.com>
2020-02-28 23:39:49 +0100

    test server side

--- tests/testthat/test-selectInput_modules.R
+++ tests/testthat/test-selectInput_modules.R
@@ -43,3 +43,115 @@ test_that("verdi_ui", {
     expect_equal(function_name("testingMore")$attribs$id, paste0("testingMore-", id))
     expect_error(function_name("test1", "test2"))
 })
+
+test_that("rad1_server", {
+    id <- "rad1"
+    function_name <- get(paste0(id, "_server"))
+
+    shiny::testModule(function_name, {
+        expect_equal_to_reference(output$rad1,
+                                  paste0("data/module_", "rad1", "1.rds")
+                                  )
+    }, pickable = c("abc", "def", "ijk", "lmn"), default = "abc")
+
+    shiny::testModule(function_name, {
+        expect_equal_to_reference(output$rad1,
+                                  paste0("data/module_", "rad1", "2.rds")
+                                  )
+    }, pickable = c("abc", "ijk"), default = "abc")
+
+    shiny::testModule(function_name, {
+        expect_equal_to_reference(output$rad1,
+                                  paste0("data/module_", "rad1", "3.rds")
+                                  )
+    }, pickable = c("abc", "ijk"), default = "lmn")
+
+    shiny::testModule(function_name, {
+        expect_error(output$rad1)
+    })
+
+})
+
+test_that("rad2_server", {
+    id <- "rad2"
+    function_name <- get(paste0(id, "_server"))
+
+    shiny::testModule(function_name, {
+        expect_equal_to_reference(output$rad2,
+                                  paste0("data/module_", "rad2", "1.rds")
+                                  )
+    }, pickable = c("abc", "def", "ijk", "lmn"), default = "abc")
+
+    shiny::testModule(function_name, {
+        expect_equal_to_reference(output$rad2,
+                                  paste0("data/module_", "rad2", "2.rds")
+                                  )
+    }, pickable = c("abc", "ijk"), default = "abc")
+
+    shiny::testModule(function_name, {
+        expect_equal_to_reference(output$rad2,
+                                  paste0("data/module_", "rad2", "3.rds")
+                                  )
+    }, pickable = c("abc", "ijk"), default = "lmn")
+
+    shiny::testModule(function_name, {
+        expect_error(output$rad2)
+    })
+
+})
+
+test_that("kolonner_server", {
+    id <- "kolonner"
+    function_name <- get(paste0(id, "_server"))
+
+    shiny::testModule(function_name, {
+        expect_equal_to_reference(output$kolonner,
+                                  paste0("data/module_", "kolonner", "1.rds")
+                                  )
+    }, pickable = c("abc", "def", "ijk", "lmn"), default = "abc")
+
+    shiny::testModule(function_name, {
+        expect_equal_to_reference(output$kolonner,
+                                  paste0("data/module_", "kolonner", "2.rds")
+                                  )
+    }, pickable = c("abc", "ijk"), default = "abc")
+
+    shiny::testModule(function_name, {
+        expect_equal_to_reference(output$kolonner,
+                                  paste0("data/module_", "kolonner", "3.rds")
+                                  )
+    }, pickable = c("abc", "ijk"), default = "lmn")
+
+    shiny::testModule(function_name, {
+        expect_error(output$kolonner)
+    })
+
+})
+
+test_that("verdi_server", {
+    id <- "verdi"
+    function_name <- get(paste0(id, "_server"))
+
+    shiny::testModule(function_name, {
+        expect_equal_to_reference(output$verdi,
+                                  paste0("data/module_", "verdi", "1.rds")
+                                  )
+    }, pickable = c("abc", "def", "ijk", "lmn"), default = "abc")
+
+    shiny::testModule(function_name, {
+        expect_equal_to_reference(output$verdi,
+                                  paste0("data/module_", "verdi", "2.rds")
+                                  )
+    }, pickable = c("abc", "ijk"), default = "abc")
+
+    shiny::testModule(function_name, {
+        expect_equal_to_reference(output$verdi,
+                                  paste0("data/module_", "verdi", "3.rds")
+                                  )
+    }, pickable = c("abc", "ijk"), default = "lmn")
+
+    shiny::testModule(function_name, {
+        expect_error(output$verdi)
+    })
+
+})

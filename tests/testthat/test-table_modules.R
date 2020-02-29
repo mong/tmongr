18c9761bc7c89ad565a8bd7b0b0482c32fe50e39
arnfinn <arnfinn.steindal@gmail.com>
2020-02-29 22:04:04 +0100

    was testing too much

--- tests/testthat/test-table_modules.R
+++ tests/testthat/test-table_modules.R
@@ -2,6 +2,6 @@ context("tableModules")
 
 test_that("tab_panel_server", {
     shiny::testModule(tab_panel_server, {
-        expect_equal_to_reference(output$tabeller, "data/module_tab_panel1.rds")
+        expect_equal_to_reference(output$tabeller[["html"]], "data/module_tab_panel1.rds")
     })
 })

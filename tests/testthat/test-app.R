7e93d78d32741782e14fe7b6b952596253fefef5
arnfinn <arnfinn.steindal@gmail.com>
2020-03-16 14:59:53 +0100

    test with grepl instead of equal_to_reference

--- tests/testthat/test-app.R
+++ tests/testthat/test-app.R
@@ -1,3 +1,28 @@
 test_that("app_ui", {
-    expect_equal_to_reference(app_ui(), "data/app_ui.rds")
+  ref_chr <- as.character(purrr::flatten(app_ui()))[4]
+  print(ref_chr)
+  expect_true(grepl("www/hn.png", ref_chr, fixed = TRUE))
+  expect_true(grepl("color: #003A8C", ref_chr, fixed = TRUE))
+  expect_true(grepl("color: #808080", ref_chr, fixed = TRUE))
+  expect_true(grepl("max-width: 1200px", ref_chr, fixed = TRUE))
+  expect_true(grepl("Pasientstrømmer, Helse Nord RHF", ref_chr, fixed = TRUE))
+  expect_true(grepl("just_overf", ref_chr, fixed = TRUE))
+  expect_true(grepl("behandlingsniva", ref_chr, fixed = TRUE))
+  expect_true(grepl("hastegrad1", ref_chr, fixed = TRUE))
+  expect_true(grepl("keep_names", ref_chr, fixed = TRUE))
+  expect_true(grepl("Alle kontakter", ref_chr, fixed = TRUE))
+  expect_true(grepl("Poliklinikk", ref_chr, fixed = TRUE))
+  expect_true(grepl("alder", ref_chr, fixed = TRUE))
+  expect_true(grepl("hastegrad2", ref_chr, fixed = TRUE))
+  expect_true(grepl("aar", ref_chr, fixed = TRUE))
+  expect_true(grepl("shiny-html-output", ref_chr, fixed = TRUE))
+  expect_true(grepl("Informasjon", ref_chr, fixed = TRUE))
+  expect_true(grepl("Dataene som presenteres er fra Norsk pasientregister", ref_chr, fixed = TRUE))
+  expect_true(grepl("Ratene er beregnet ut i fra befolkningstall fra SSB", ref_chr, fixed = TRUE))
 })
+
+#test_that("server_ui", {
+#  shiny::testModule(app_server, {
+#    expect_equal_to_reference(output, "test.rds")
+#  })
+#})

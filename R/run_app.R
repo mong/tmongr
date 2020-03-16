0697e876168223e24d7140ad454c1ee9e9492f90
arnfinn <arnfinn.steindal@gmail.com>
2020-03-16 14:08:51 +0100

    polish

--- R/run_app.R
+++ R/run_app.R
@@ -3,14 +3,13 @@
 #' @export
 #'
 run_app <- function() {
+
   shiny::addResourcePath(
     "www", system.file("app/www", package = getPackageName())
-  )
-  shiny::addResourcePath(
-    "Rmd", system.file("app/Rmd", package = getPackageName())
   )
 
   shiny::shinyApp(
     ui = app_ui, server = app_server
   )
+
 }

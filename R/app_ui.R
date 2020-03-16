2d4e3067452de79f1d886e13ee14bc44e1f97650
arnfinn <arnfinn.steindal@gmail.com>
2020-03-16 14:40:23 +0100

    add shiny:: to app_ui

--- R/app_ui.R
+++ R/app_ui.R
@@ -3,32 +3,32 @@
 #' @return user interface
 #' @export
 app_ui <- function() {
-  fluidPage(theme = shinythemes::shinytheme("cerulean"),
-            titlePanel(tags$head(tags$link(rel = "icon", type = "image/png", href = "www/hn.png"),
-                                 tags$title("Pasientstrømmer, Helse Nord RHF"),
+  shiny::fluidPage(theme = shinythemes::shinytheme("cerulean"),
+            shiny::titlePanel(shiny::tags$head(shiny::tags$link(rel = "icon", type = "image/png", href = "www/hn.png"),
+                                 shiny::tags$title("Pasientstrømmer, Helse Nord RHF"),
                                  # Farge på linker, samt text inaktive faner
-                                 tags$style(type = "text/css", "a{color: #808080;}"),
+                                 shiny::tags$style(type = "text/css", "a{color: #808080;}"),
                                  # Farge på tittel
-                                 tags$style(type = "text/css", "h1{color: #003A8C;}"),
+                                 shiny::tags$style(type = "text/css", "h1{color: #003A8C;}"),
                                  # max bredde på side
-                                 tags$style(type = "text/css", ".container-fluid {  max-width: 1200px;}")
+                                 shiny::tags$style(type = "text/css", ".container-fluid {  max-width: 1200px;}")
 
             )
             ),
 
-            uiOutput("figurtekst"),
-            uiOutput("lastned"),
-            br(),
-            sidebarPanel(
+            shiny::uiOutput("figurtekst"),
+            shiny::uiOutput("lastned"),
+            shiny::br(),
+            shiny::sidebarPanel(
               dynamiskTabellverk:::common_ui("just_overf"),
-              uiOutput("valg"),
+              shiny::uiOutput("valg"),
               dynamiskTabellverk:::common_ui("rad1"),
               dynamiskTabellverk:::common_ui("rad2"),
-              uiOutput("hdg"),
+              shiny::uiOutput("hdg"),
               dynamiskTabellverk:::common_ui("kolonner"),
               dynamiskTabellverk:::common_ui("verdi"),
-              br(),
-              uiOutput("filter"),
+              shiny::br(),
+              shiny::uiOutput("filter"),
               dynamiskTabellverk:::common_ui("bo"),
               dynamiskTabellverk:::common_ui("beh"),
               dynamiskTabellverk:::common_ui("aar"),
@@ -37,14 +37,14 @@ app_ui <- function() {
               dynamiskTabellverk:::common_ui("hastegrad2"),
               dynamiskTabellverk:::common_ui("alder"),
               dynamiskTabellverk:::common_ui("kjonn"),
-              br(),
-              uiOutput("instilling"),
+              shiny::br(),
+              shiny::uiOutput("instilling"),
               dynamiskTabellverk:::common_ui("prosent"),
               dynamiskTabellverk:::common_ui("keep_names"),
               dynamiskTabellverk:::common_ui("snitt"),
               width = 3
             ),
-            mainPanel(
+            shiny::mainPanel(
                 shiny::tabsetPanel(type = "tabs", id = "tab",
                      shiny::tabPanel("Alle kontakter", shiny::tableOutput("alle"), value = "alle"),
                      shiny::tabPanel("Døgnopphold", shiny::tableOutput("dogn"), value = "dogn"),

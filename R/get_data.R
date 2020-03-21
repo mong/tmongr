816faee163bb1ef1239bab4d48bda668fe793392
arnfinn <arnfinn.steindal@gmail.com>
2020-03-21 10:22:25 +0100

    lintr

--- R/get_data.R
+++ R/get_data.R
@@ -4,10 +4,10 @@ get_data <- function() {
   } else if (file.exists("../tabellverk/data/behandler.rds")) {
     uten_overf <- readRDS("../tabellverk/data/behandler.rds")
     med_overf <- readRDS("../tabellverk/data/justertoverf.rds")
-    
+
     uten_overf$niva <- "Uten overf"
     med_overf$niva <- "Med overf"
-    
+
     return(rbind(uten_overf, med_overf))
   } else {
     return(tmongr::testdata3)

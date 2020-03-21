1da2bd7fdb5c4d7a44facc813ad349fd5c36e42a
arnfinn <arnfinn.steindal@gmail.com>
2020-03-21 10:20:51 +0100

    get_data update
    Use locally saved files if they exist

--- R/get_data.R
+++ R/get_data.R
@@ -1,7 +1,15 @@
 get_data <- function() {
-    if (!exists("datasett")) {
-        return(tmongr::testdata3)
-    } else {
-        return(datasett)
-    }
+  if (exists("datasett")) {
+    return(datasett)
+  } else if (file.exists("../tabellverk/data/behandler.rds")) {
+    uten_overf <- readRDS("../tabellverk/data/behandler.rds")
+    med_overf <- readRDS("../tabellverk/data/justertoverf.rds")
+    
+    uten_overf$niva <- "Uten overf"
+    med_overf$niva <- "Med overf"
+    
+    return(rbind(uten_overf, med_overf))
+  } else {
+    return(tmongr::testdata3)
+  }
 }

b088d2a4669a831949c3ca3923d314c0ce0a09e5
arnfinn <arnfinn.steindal@gmail.com>
2020-03-23 14:54:03 +0100

    testing get_data with files on disk

--- tests/testthat/test-get_data.R
+++ tests/testthat/test-get_data.R
@@ -1,6 +1,22 @@
-test_that("get_data", {
-  testthat::expect_equal(get_data(), testdata3)
+test_that("get_data with files on disk", {
+
+  saveRDS(testdata, file = "behandler.rds")
+  expect_error(suppressWarnings(get_data()))
+
+  saveRDS(testdata2, file = "justertoverf.rds")
+  expect_error(get_data())
+  file.remove("justertoverf.rds")
+
+  saveRDS(testdata, file = "justertoverf.rds")
+  expect_equal_to_reference(get_data(), "data/get_data.rds")  
+
+  file.remove("behandler.rds")
+  file.remove("justertoverf.rds")
+})
+
+test_that("get_data without files on disk", {
+  expect_equal(get_data(), testdata3)
 
   datasett <<- "qwerty"
-  testthat::expect_equal(get_data(), "qwerty")
+  expect_equal(get_data(), "qwerty")
 })

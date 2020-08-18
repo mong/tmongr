8bf767d16c6737d6bd1154e3c9f1c9b2255fa28c
arnfinn <arnfinn.steindal@gmail.com>
2020-08-18 16:23:41 +0200

    read csv directly

--- data-raw/test-run.R
+++ data-raw/test-run.R
@@ -3,12 +3,24 @@ rm(list=ls())
 
 getwd()
 
-uten_overf <- readRDS("../tabellverk/data/behandler.rds")
-med_overf <- readRDS("../tabellverk/data/justertoverf.rds")
+uten_overf <- read.table('../tmongrdata/avd_behandlert19.csv', 
+                         sep = ",", 
+                         header=T, 
+                         encoding = 'UTF-8', 
+                         stringsAsFactors = FALSE)
+
+med_overf <- read.table('../tmongrdata/avd_justoverft19.csv', 
+                        sep = ",", 
+                        header=T, 
+                        encoding = 'UTF-8', 
+                        stringsAsFactors = FALSE)
+
 
 uten_overf$niva <- "Uten overf"
 med_overf$niva <- "Med overf"
 
 datasett <- rbind(uten_overf, med_overf)
+
+names(datasett) <- tolower(names(datasett))
 
 run_app()

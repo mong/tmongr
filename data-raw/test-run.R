0cbed2ef5c3c09ad912afd22db54c9c2929f5823
arnfinn <arnfinn.steindal@gmail.com>
2022-04-12 13:01:42 +0200

    update test-run

--- data-raw/test-run.R
+++ data-raw/test-run.R
@@ -1,26 +1,25 @@
-
 rm(list = ls())
 
 getwd()
 
-uten_overf <- readRDS("../tabellverk/data/behandler.rds")
-med_overf <- readRDS("../tabellverk/data/justertoverf.rds")
-
-uten_overf$niva <- "Uten overf"
-med_overf$niva <- "Med overf"
-
-datasett <- rbind(uten_overf, med_overf)
-
-run_app()
-
-
 datasett <- read.table("../tmongrdata/fag.csv",
-  sep = ",",
-  header = T,
-  encoding = "UTF-8",
-  stringsAsFactors = FALSE
+                       sep = ",",
+                       header = T,
+                       encoding = "UTF-8",
+                       stringsAsFactors = FALSE
 )
 
 names(datasett) <- tolower(names(datasett))
+
+datasett2 <- read.table("../tmongrdata/fag2.csv",
+                        sep = ",",
+                        header = T,
+                        encoding = "UTF-8",
+                        stringsAsFactors = FALSE)
+
+names(datasett2) <- tolower(names(datasett2))
+datasett2$niva <- "Uten overf"
+datasett$niva <- "Med overf"
+datasett <- rbind(datasett2, datasett)
 
 run_app()

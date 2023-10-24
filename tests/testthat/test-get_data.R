dc3b200e2f4815dabe6e32bbdd468169103c2657
arnfinn <arnfinn.steindal@gmail.com>
2023-10-24 15:20:57 +0200

    test lesing av csv-filer

--- tests/testthat/test-get_data.R
+++ tests/testthat/test-get_data.R
@@ -16,3 +16,14 @@ test_that("get_data with files on disk", {
 test_that("get_data without files on disk", {
   expect_equal(get_data(), testdata3)
 })
+
+test_that("get_data with defined csv file", {
+  write.csv(testdata, file = "fag.csv", row.names = FALSE)
+  expect_equal(get_data(), testdata)
+  write.csv(testdata, file = "fag2.csv", row.names = FALSE)
+  expect_equal_to_reference(get_data(), "data/get_data.rds")
+  file.remove("fag.csv")
+  # test uten fag.csv, men kun med fag2.csv. Da skal den ikke hente data.
+  expect_equal(get_data(), testdata3)
+  file.remove("fag2.csv")
+})
